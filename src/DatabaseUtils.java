e7c3ae1f793a67afdbf2a750f2ec3b753fca1f6f
Mohammd Shihab Hossain <68942280+Zul-Qarnain@users.noreply.github.com>
2025-01-16 11:19:02 +0600

    Update DatabaseUtils.java

--- src/DatabaseUtils.java
+++ src/DatabaseUtils.java
@@ -4,22 +4,17 @@ import javax.swing.*;
 
 public class DatabaseUtils {
 
-    private static Connection dbConnection;
-
     public static Connection getConnection() throws SQLException {
-        if (dbConnection == null || dbConnection.isClosed()) {
-            try {
-                String jdbcUrl = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-                String username = "System";
-                String password = "sysmain123";
+        try {
+            String jdbcUrl = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+            String username = "System";
+            String password = "sysmain123";
 
-                DriverManager.registerDriver(new OracleDriver());
-                dbConnection = DriverManager.getConnection(jdbcUrl, username, password);
-            } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, "Error connecting to the database: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                throw e;
-            }
+            DriverManager.registerDriver(new OracleDriver());
+            return DriverManager.getConnection(jdbcUrl, username, password);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error connecting to the database: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            throw e;
         }
-        return dbConnection;
     }
 }
